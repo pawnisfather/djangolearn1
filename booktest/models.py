613b94132b1cfe4bd294f4c0524c5a52abf14ada
root <admin@example.com>
2019-06-25 10:27:19 +0800

    第二次提交

---
+++ booktest/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+# Create your models here.
+class BookInfo(models.Model):
+    b_title = models.CharField(max_length=20)
+    b_pub_date = models.DateField()
+    def __str__(self):
+        return self.b_title
+
+
+class HeroInfo(models.Model):
+    h_name = models.CharField(max_length=10)
+    h_gender = models.BooleanField()
+    h_content = models.CharField(max_length=1000)
+    h_book = models.ForeignKey(BookInfo, on_delete=True)
+
