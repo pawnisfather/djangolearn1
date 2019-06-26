2b508916c8bbf6c9e00b281b1361eb9551bb469a
root <admin@example.com>
2019-06-26 18:13:40 +0800

    6/26/model功能体验

--- booktest/admin.py
+++ booktest/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
+from .models import *
+
+
 # Register your models here.
+class BookInfoAdmin(admin.ModelAdmin):
+    list_display = ['id', 'b_title', 'b_pub_date']
+    list_filter = ['b_pub_date']
+    search_fields = ['b_title']
+    fieldsets = [
+        ('base', {'fields': ['b_title']}),
+        ('more', {'fields': ['b_pub_date']}),
+
+    ]
+
+
+admin.site.register(BookInfo, BookInfoAdmin)
+admin.site.register(HeroInfo)
--- booktest/models.py
+++ booktest/models.py
@@ -4,7 +4,8 @@ from django.db import models
 # Create your models here.
 class BookInfo(models.Model):
     b_title = models.CharField(max_length=20)
-    b_pub_date = models.DateField()
+    b_pub_date = models.DateTimeField()
+
     def __str__(self):
         return self.b_title
 
@@ -15,3 +16,5 @@ class HeroInfo(models.Model):
     h_content = models.CharField(max_length=1000)
     h_book = models.ForeignKey(BookInfo, on_delete=True)
 
+    def __str__(self):
+        return self.h_name
