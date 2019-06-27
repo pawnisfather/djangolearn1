cc22514847724beff7913f8eb16d69534aae6cb3
root <admin@example.com>
2019-06-27 18:16:38 +0800

    6/27/view功能体验

--- booktest/admin.py
+++ booktest/admin.py
@@ -3,16 +3,23 @@ from django.contrib import admin
 from .models import *
 
 
+class HeroInfoInline(admin.TabularInline):
+    model = HeroInfo
+    extra = 2
+
+
 # Register your models here.
 class BookInfoAdmin(admin.ModelAdmin):
     list_display = ['id', 'b_title', 'b_pub_date']
     list_filter = ['b_pub_date']
     search_fields = ['b_title']
+    list_per_page = 5
     fieldsets = [
         ('base', {'fields': ['b_title']}),
         ('more', {'fields': ['b_pub_date']}),
 
     ]
+    inlines = [HeroInfoInline, ]
 
 
 admin.site.register(BookInfo, BookInfoAdmin)
--- booktest/views.py
+++ booktest/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import *
+
 
 # Create your views here.
+def index(request):
+    print(request.get_port())
+    return HttpResponse('hello world')
