f061fe0aba9f19c7b164bcdc1656b3ae6fda7e61
root <admin@example.com>
2019-06-28 09:16:01 +0800

    6/29/view功能体验

--- booktest/views.py
+++ booktest/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import *
+from django.template import loader, RequestContext
+from .models import *
 
 
 # Create your views here.
 def index(request):
-    print(request.get_port())
-    return HttpResponse('hello world')
+    book_list = BookInfo.objects.all()
+    context = {'title': book_list}
+    return render(request, 'booktest/index.html', context)
